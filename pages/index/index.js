ff360d8c84e6f2fcbb6e57318e652089ef8472f5
ColaZZ <zlinxx@foxmail.com>
2017-01-22 17:29:57 +0800

    index首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,33 @@
+var API_URL = "https://api.douban.com/v2/movie/top250";
 Page({
-  data: {},
+  data: {
+    title:"加载中...",
+    movies:[]
+  },
+  onLoad: function(){
+    var that = this;
+    wx.showToast({
+      title:"加载中",
+      icon:"loading",
+      duration: 10000
+    })
+    wx.request({
+      url: API_URL,
+      data: {},
+      header:{
+        "Content-Type":"json"
+      },
+      success: function(res){
+        // success
+        var data = res.data;
+        console.log(data);
+        that.setData({
+             title:data.title,
+             movies:data.subjects
+        })
+        wx.hideToast();
+        //console.log(res.data);
+      }
+    })
+  }
 })
